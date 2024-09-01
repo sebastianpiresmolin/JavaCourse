670b93fab4bbe6c7d92cc0ca6b3317dd8e0e4c43
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-09-01 12:57:24 +0200

    start work on refactoring. Starting by creating Obstacle interface and using it to create walls

---
+++ TextGame/src/Obstacle.java
@@ -0,0 +1,5 @@
+public interface Obstacle {
+    int getX();
+
+    int getY();
+}
---
+++ TextGame/src/Wall.java
@@ -0,0 +1,19 @@
+public class Wall implements Obstacle {
+    private final int x;
+    private final int y;
+
+    public Wall(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+}
