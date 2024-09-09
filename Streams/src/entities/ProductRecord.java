e2c0ec52b2dcfc922226b788c05891b9ede45d69
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-09-09 21:50:56 +0200

    add record

--- Streams/src/entities/ProductRecord.java
+++ Streams/src/entities/ProductRecord.java
@@ -1,8 +1,29 @@
 package entities;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProductRecord {
-    
+    private List<Product> products;
+
+    public ProductRecord(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public Product getProductById(int id) {
+        for (var product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        System.out.println("Product not found");
+        return null;
+    }
 }
